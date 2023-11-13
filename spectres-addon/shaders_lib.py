ab4c3eb988407c31205ab8aa4b9b5d4f1d4b1b21
nicolas mars <nicolasmars@protonmail.com>
2023-11-13 10:56:01 +0100

    add panel in node editor to unlink instances for repetitions

--- spectres-addon/shaders_lib.py
+++ spectres-addon/shaders_lib.py
@@ -10,12 +10,12 @@ module = sys.modules[__name__]
 program = None
 loader = None
 
-# ---------------------------------------------------------- Blender Class
-class SL_PT_panel(bpy.types.Panel):
+# ---------------------------------------------------------- LOADER
+class SL_PT_ui_panel(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_parent_id = "VIEW3D_PT_sp_panel"
-    bl_idname = "VIEW3D_PT_sl_panel"
+    bl_idname = "VIEW3D_PT_sl_ui_panel"
     bl_label = "Shaders Lib"
 
     def draw(self, context):
@@ -57,7 +57,6 @@ class SL_PT_props(bpy.types.PropertyGroup):
         default = 1,
         options ={'SKIP_SAVE'},
         update=on_exemples)
-
     
 class SL_OT_loader(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
@@ -98,7 +97,6 @@ class SL_OT_loader(bpy.types.Operator):
         [a.tag_redraw() for a in context.screen.areas]
         return {'FINISHED'}
 
-
 class SL_OT_load(SL_OT_loader):
     bl_idname = "scene.sl_load"
     bl_label = "Load Shader Lib"
@@ -111,9 +109,133 @@ class SL_OT_unload(SL_OT_loader):
     def invoke(self, context, event): return context.window_manager.invoke_confirm(self, event)
 
 
+# ---------------------------------------------------------- NODE TOOLS
+
+nodes_log = "nodes log"
+
+
+def get_instance(node_tree, tree_list):
+    for node in node_tree.nodes :
+        if node.type == 'GROUP':
+            if node.node_tree : 
+                split_name = node.node_tree.name.split(".")
+                i1 = max(len(split_name)-1,0)
+                i2 = max(len(split_name)-2,0)
+
+                if split_name[i1] == 'Instance' or split_name[i2] == 'Instance':
+                    tree_list.append(node)
+                    print("Found Instance node : ", node.node_tree.name)
+
+                get_instance(node.node_tree, tree_list)
+
+def check_set_instancer(node_tree):
+    instance_list = []
+    get_instance(tree, instance_list)
+    label = "Found " + str(len(instance_list)) + " instances in this tree"
+    if len(instance_list) > 0:
+        users = instance_list[0].users 
+        if users == len(instance_list) + 1: is_instantiated = True
+        elif users > len(instance_list) + 1: is_instantiated = True
+        print("instance_list[0].users : ", instance_list[0].users)
+
+def clear_node_tree(node_tree):
+    for node in node_tree.nodes :
+        if node.type == 'GROUP':  
+            if node.node_tree : 
+                clear_node_tree(node.node_tree)
+                bpy.data.node_groups.remove(node.node_tree)
+
+def is_node_tree_selected():
+    if bpy.context.selected_nodes and bpy.context.scene.sl_props.is_loaded :
+        if hasattr(bpy.context.selected_nodes[0], "node_tree"): return True
+    return False
+
+
+class SL_PT_node_panel(bpy.types.Panel):
+    bl_space_type = 'NODE_EDITOR'
+    bl_region_type = 'UI'
+    bl_idname = "VIEW3D_PT_sl_node_panel"
+    bl_label = "~ ENCI | TOOLS ~"
+    bl_category = "SPECTRES"
+
+    def draw(self, context):
+        scene = context.scene
+        layout = self.layout
+        row = layout.row()
+        
+        row = layout.row()
+        row.operator(SL_OT_instancer_check.bl_idname, text = "Check tree for instance")
+        
+        label = nodes_log
+        box = layout.box()
+        box.label(text = label)
+
+class SL_OT_instancer(bpy.types.Operator):
+    bl_options = {'REGISTER', 'UNDO'}
+    bl_context = "Scene"
+
+class SL_OT_instancer_check(SL_OT_instancer):
+    bl_idname = "scene.sl_node_instancer"
+    bl_label = "Check tree for instance"
+
+    def clone_linked(self, instance_list):
+        for tree in instance_list :
+            print(tree.name)
+
+    def execute(self, context) :
+
+        if is_node_tree_selected() :
+            module.nodes_log = ""
+            tree = context.selected_nodes[0].node_tree
+
+            print("tree.name :", tree.name, " tree.users : ", tree.users)
+
+            if tree.users == 1 :
+                instance_list = [] # nodes
+                get_instance(tree, instance_list)
+
+                num_instances = len(instance_list)
+                if num_instances > 0 :
+                    users = instance_list[0].node_tree.users
+
+                    if users == num_instances + 1:
+                        text = "tree is already instiated, all set and ready to shape"
+
+                        module.nodes_log += text
+                        print(text)
+                    
+                    elif users == num_instances :
+                        text = "internal Instances exists, you need to have one at top level"
+                        print(text)
+                        module.nodes_log += text
+
+                    elif users > num_instances + 1 :
+                        text = "making single user !"
+                        print(text)
+                        new_tree_instance = instance_list[0].node_tree.copy()
+                        new_tree_instance.name = tree.name + ".Instance"
+
+                        for i in range(0, len(instance_list), 1) :
+                            print("instance_list[i].name : ", instance_list[i].name)
+                            instance_list[i].node_tree = new_tree_instance
+
+                        module.nodes_log += text
+                  
+            else :
+                text = "The Instancer Node must be a unique user"
+                print(text)
+                module.nodes_log += text
+
+        return {'FINISHED'}
+
 
 # ---------------------------------------------------- Module registration
-classes = [SL_PT_panel, SL_PT_props, SL_OT_load, SL_OT_unload]
+classes = [SL_PT_ui_panel, 
+           SL_PT_props, 
+           SL_OT_load, 
+           SL_OT_unload,
+           SL_PT_node_panel,
+           SL_OT_instancer_check]
 
 def register():
     for cls in classes:
